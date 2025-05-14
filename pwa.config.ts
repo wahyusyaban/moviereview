53dd8d0030ae6f108a85091ba2c47dd4627a9e7e
wahyusyaban <48669571+wahyusyaban@users.noreply.github.com>
2025-05-14 20:20:19 +0800

    Update pwa.config.ts

--- pwa.config.ts
+++ pwa.config.ts
@@ -20,13 +20,13 @@ export default {
       name: 'Shortcut 1',
       shortName: 'Shortcut 1',
       description: 'Shortcut 1 description goes here...',
-      url: '/search/label/shortcut-1?utm_source=homescreen',
+      url: '/search/label/Horror Movie?utm_source=homescreen',
     },
     {
       name: 'Shortcut 2',
       shortName: 'Shortcut 2',
       description: 'Shortcut 2 description goes here...',
-      url: '/search/label/shortcut-2?utm_source=homescreen',
+      url: '/search/label/Action Movie?utm_source=homescreen',
     },
   ],
   pwa: {
